eddce249a2390d02fa93ab600f1d8ccb3ae4b6a4
Elechim01 <mikymanny015@gmail.com>
2021-05-23 23:10:53 +0200

    Initial Commit

---
+++ Shared/CustomSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSliderApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
